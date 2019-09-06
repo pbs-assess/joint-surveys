52a821ef7a09a93cd71053e003122af3ad6a3f78
Sean Anderson <sean@seananderson.ca>
2019-09-06 15:41:21 -0700

    Add more plots; fix data extraction

--- yelloweye.R
+++ yelloweye.R
@@ -10,22 +10,21 @@ dir.create("figs", showWarnings = FALSE)
 if (file.exists("../../gfs/report/data-cache/yelloweye-rockfish.rds")) {
   d <- readRDS("../../gfs/report/data-cache/yelloweye-rockfish.rds")
   d <- d$survey_sets
+  d <- dplyr::filter(d, survey_abbrev %in% c("HBLL OUT N", "HBLL OUT S"))
 } else {
-  stop()
-  d <- gfdata::get_survey_sets("yelloweye rockfish", ssid = )
+  d <- gfdata::get_survey_sets("yelloweye rockfish", ssid = c(22, 36))
 }
-d <- dplyr::filter(d, survey_abbrev %in% c("HBLL OUT N", "HBLL OUT S"))
-d <- select(
-  d, survey_abbrev, year, longitude, latitude, density_ppkm2,
+d <- d %>% select(
+  survey_abbrev, year, longitude, latitude, density_ppkm2,
   grouping_code, depth_m
 ) %>%
   rename(survey = survey_abbrev) %>%
   mutate(density_1000ppkm2 = density_ppkm2 / 1000)
 
+# log and scale the depth predictor:
 d$depth_log <- log(d$depth_m)
 d$depth_centred <- d$depth_log - mean(d$depth_log)
 d$depth_scaled <- d$depth_centred / sd(d$depth_centred)
-
 d_utm <- convert2utm(d, coords = c("longitude", "latitude"))
 
 ggplot(d_utm, aes(X, Y,
@@ -44,13 +43,14 @@ south_grid_utm <- convert2utm(south_grid)
 
 years <- sort(unique(d_utm$year))
 north_grid_utm <- expand_prediction_grid(north_grid_utm, years = years) %>%
-  mutate(survey = "HBLL OUT N")
-south_grid_utm <- expand_prediction_grid(south_grid_utm, years = years) %>%
-  mutate(survey = "HBLL OUT S")
-
-joint_grid_utm <- dplyr::bind_rows(north_grid_utm, south_grid_utm) %>%
+  mutate(survey = "HBLL OUT N") %>%
   mutate(depth_centred = log(depth) - mean(d$depth_log)) %>%
   mutate(depth_scaled = depth_centred / sd(d$depth_centred))
+south_grid_utm <- expand_prediction_grid(south_grid_utm, years = years) %>%
+  mutate(survey = "HBLL OUT S") %>%
+  mutate(depth_centred = log(depth) - mean(d$depth_log)) %>%
+  mutate(depth_scaled = depth_centred / sd(d$depth_centred))
+joint_grid_utm <- dplyr::bind_rows(north_grid_utm, south_grid_utm)
 
 sp <- make_spde(d_utm$X, d_utm$Y, n_knots = 175)
 plot_spde(sp)
@@ -144,6 +144,36 @@ ggplot(ind, aes(year, est * scale)) + geom_line() +
   xlab("Year") + ylab("Estimated density (1000s of fish)") +
   geom_vline(xintercept = seq(2006, 2018, 2), lty = 2, alpha = 0.2)
 ggsave("figs/hbll-index.pdf", width = 8, height = 5)
+
+# what about the individual surveys? -----------------------
+
+pred_north <- predict(m,
+  newdata = north_grid_utm,
+  return_tmb_object = TRUE, xy_cols = c("X", "Y")
+)
+ind_north <- get_index(pred_north, bias_correct = FALSE)
+
+pred_south <- predict(m,
+  newdata = south_grid_utm,
+  return_tmb_object = TRUE, xy_cols = c("X", "Y")
+)
+ind_south <- get_index(pred_south, bias_correct = FALSE)
+
+ind_north$type <- "HBLL N"
+ind_south$type <- "HBLL S"
+ind$type <- "HBLL N + S"
+
+bind_rows(ind_north, ind_south) %>%
+  bind_rows(ind) %>%
+  ggplot(aes(year, est * scale)) + geom_line(aes(colour = type)) +
+  geom_ribbon(aes(ymin = lwr * scale, ymax = upr * scale, fill = type), alpha = 0.4, colour = NA) +
+  facet_wrap(~type, ncol = 1) +
+  xlab("Year") + ylab("Estimated density (1000s of fish)") +
+  geom_vline(xintercept = seq(2006, 2018, 2), lty = 2, alpha = 0.2) +
+  scale_color_brewer(palette = "Set2") +
+  scale_fill_brewer(palette = "Set2")
+ggsave("figs/hbll-index-components.pdf", width = 5.5, height = 8)
+
 
 # # -----------------------
 #
